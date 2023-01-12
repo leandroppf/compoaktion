194c08e12b13b3fe984e9db1675e28b7addd2fba
Leandro Pedroso <lpedroso@meudroz.com>
2023-01-12 09:46:17 -0300

    Configurações para o build da lib

---
+++ src/library/index.ts
@@ -0,0 +1,8 @@
+export { Button } from "./Button/Button";
+export type { ButtonProps } from "./Button/Button";
+
+export { default as Checkbox } from "./Checkbox/Checkbox";
+export type {
+  CheckboxRootProps,
+  CheckboxIndicatorProps,
+} from "./Checkbox/Checkbox";
